f2d31355c5e3bb7c973d9455b52ddc922a5a1c2c
Clurquiz <clementine.urquizar@gmail.com>
2018-01-28 00:20:07 +0100

    Upd : script -> get test sentence

--- add_line.sh
+++ add_line.sh
@@ -1,4 +1,7 @@
-str_test="titi"
+str_test=`grep 'ft_printf' < conv_bigc/a01_basic_01.c| cut -d "(" -f 2 | tr -d ');' | tr -d '"'`
+str_test=\"$str_test\"
+echo $str_test
+
 sed "s/ft_strdel(&data.s1);/ft_write_debug($str_test, data, ret);\\
 	ft_strdel(\&data.s1);/"\
 	conv_bigc/a01_basic_01.c
