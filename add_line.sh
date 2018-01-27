ebd7e2149c09a550a4504f29730537cd1297eca1
Clurquiz <clementine.urquizar@gmail.com>
2018-01-27 23:55:47 +0100

    Add : sed ok

--- add_line.sh
+++ add_line.sh
@@ -1,5 +1,4 @@
-#sed -i 's/ft_strdel(\&data.s1);/ft_write_debug(\"toto\", data, ret);\nft_strdel(\&data.s1);/' "$1"
-sed -i 's/ft_strdel(\&data.s1);/toto;\nft_strdel(\&data.s1);/' "$1"
-#ft_strdel(&data.s1);
-#ft_write_debug("toto", data, ret);
-
+str_test="titi"
+sed "s/ft_strdel(&data.s1);/ft_write_debug($str_test, data, ret);\\
+	ft_strdel(\&data.s1);/"\
+	conv_bigc/a01_basic_01.c
