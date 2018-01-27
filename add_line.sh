b2a15c32669c9e703a45550a37652000aeb3220f
Clurquiz <clementine.urquizar@gmail.com>
2018-01-28 00:28:35 +0100

    Upd : sed -i without backup

--- add_line.sh
+++ add_line.sh
@@ -2,6 +2,6 @@ str_test=`grep 'ft_printf' < conv_bigc/a01_basic_01.c| cut -d "(" -f 2 | tr -d '
 str_test=\"$str_test\"
 echo $str_test
 
-sed "s/ft_strdel(&data.s1);/ft_write_debug($str_test, data, ret);\\
+sed -i '' "s/ft_strdel(&data.s1);/ft_write_debug($str_test, data, ret);\\
 	ft_strdel(\&data.s1);/"\
 	conv_bigc/a01_basic_01.c
