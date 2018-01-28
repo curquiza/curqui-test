a6e1fc10af78b539c6cdcb7a5faa911486bb53bd
Clurquiz <clementine.urquizar@gmail.com>
2018-01-28 02:24:51 +0100

    Upd : script ok

--- add_line.sh
+++ add_line.sh
@@ -1,10 +1,17 @@
-str_test=`grep 'ft_printf' < conv_bigc/a01_basic_01.c | cut -d "(" -f 2 | tr -d ');' | tr -d '"'`
-str_test=\"$str_test\"
-echo "ft_write_debug($str_test, data, ret);"
+insert_line() {
+	str_test=`grep 'ft_printf' < $1 | cut -d "(" -f 2 | tr -d ');' | tr -d '"'`
+	str_test=\"$str_test\"
 
-sed -i '' "s/ft_strdel(&data.s1);/ft_write_debug($str_test, data, ret);\\
-	ft_strdel(\&data.s1);/"\
-	conv_bigc/a01_basic_01.c
+	echo "ft_write_debug($str_test, data, ret); in $1"
 
+	sed -i '' "s/ft_strdel(&data.s1);/ft_write_debug($str_test, data, ret);\\
+		ft_strdel(\&data.s1);/"\
+		$1
+		#conv_bigc/a01_basic_01.c
+}
+files=`find . -type d \( -name "conv_*" -o -name "basics" -o -name "percent" -o -name "mixed" \) \
+	-exec find {} -name "*.c" \;`
 
-#find . -type d \( -name "conv_*" -o -name "basics" -o -name "percent" -o -name "mixed" \) -exec find {} -name "*.c" \;
+for i in $files; do
+	insert_line $i
+done
