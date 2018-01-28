6f1e2b815a67e15a565993ddd123cbb5b32fc04f
Clurquiz <clementine.urquizar@gmail.com>
2018-01-28 01:49:28 +0100

    Upd : find

--- add_line.sh
+++ add_line.sh
@@ -1,7 +1,10 @@
-str_test=`grep 'ft_printf' < conv_bigc/a01_basic_01.c| cut -d "(" -f 2 | tr -d ');' | tr -d '"'`
+str_test=`grep 'ft_printf' < conv_bigc/a01_basic_01.c | cut -d "(" -f 2 | tr -d ');' | tr -d '"'`
 str_test=\"$str_test\"
-echo $str_test
+echo "ft_write_debug($str_test, data, ret);"
 
 sed -i '' "s/ft_strdel(&data.s1);/ft_write_debug($str_test, data, ret);\\
 	ft_strdel(\&data.s1);/"\
 	conv_bigc/a01_basic_01.c
+
+
+#find . -type d \( -name "conv_*" -o -name "basics" -o -name "percent" -o -name "mixed" \) -exec find {} -name "*.c" \;
