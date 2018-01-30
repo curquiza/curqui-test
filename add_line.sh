1fcb6050acd2cc05ee0a9bb8600ac8be72ba27cc
Clementine <clementine@MacBook-Pro-de-Clementine.local>
2018-01-30 13:52:08 +0100

    Upd : delete in progress

--- add_line.sh
+++ add_line.sh
@@ -2,18 +2,30 @@ insert_line() {
 	str_test=`grep 'ft_printf' < $1 | cut -c 22- | sed 's/);//g' | tr -d '"'`
 	str_test=\"$str_test\"
 
-	echo "ft_write_debug($str_test, data, ret); in $1"
-
 	sed -i '' "s/ft_strdel(&data.s1);/ft_write_debug($str_test, data, ret);\\
 	ft_strdel(\&data.s1);/" \
 	$1
+	echo "ft_write_debug($str_test, data, ret); in $1"
 }
 
-files=`find . -type d \( -name "conv_*" -o -name "basics" -o -name "percent" -o -name "mixed" \) \
-	-exec find {} -name "*.c" \;`
+delete_line() {
+	line=`grep "ft_write_debug" conv_bigs/a01_basic_01.c`
+	sed -i '' "s/$line//" conv_bigs/a01_basic_01.c
+}
 
-for i in $files; do
-	insert_line $i
-done
+#action_on_file() {
+	
+	#insert_line $1
+#}
 
+#files=`find . -type d \( -name "conv_*" -o -name "basics" -o -name "percent" -o -name "mixed" \) \
+#	-exec find {} -name "*.c" \;`
+
+#for i in $files; do
+#	action_on_file $i
+#done
+
+delete_line
+
+### Add '\"' but sed -i  doesn't work after the grep
 #grep 'write_debug' < conv_s/a14_all_02.c | cut -d '"' -f 2 | cut -d ',' -f 1 | sed -i '' 's/^/\\\"/;s/$/\\\"/'
