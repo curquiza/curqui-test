aa7d54cbb43a56573b03ef6c0f0cf5be9b375fbd
Clementine <clementine@MacBook-Pro-de-Clementine.local>
2018-02-02 14:02:53 +0100

    Upd : script

--- add_line.sh
+++ add_line.sh
@@ -2,16 +2,16 @@ insert_line() {
 	str_test=`grep 'ft_printf' < $1 | cut -c 22- | sed 's/);//g' | tr -d '"'`
 	str_test=\"$str_test\"
 
-	sed -i '' "s/ft_strdel(&data.s1);/ft_write_debug($str_test, data, ret);\\
-	ft_strdel(\&data.s1);/" \
+	sed -i '' -e "1,/ft_connect_stdout/ s/ft_connect_stdout;/ft_write_debug($str_test, data, ret);\\
+	ft_connect_stdout;/" \
 	$1
 	echo "ft_write_debug($str_test, data, ret); in $1"
 }
 
-delete_line() {
-	line=`grep "ft_write_debug" $1
-	sed "/$line/d" $1
-}
+#delete_line() {
+#	line=`grep "ft_write_debug" $1
+#	sed "/$line/d" $1
+#}
 
 #action_on_file() {
 	
@@ -25,7 +25,7 @@ delete_line() {
 #	action_on_file $i
 #done
 
-delete_line
+#delete_line
 
 ### Add '\"' but sed -i  doesn't work after the grep
 #grep 'write_debug' < conv_s/a14_all_02.c | cut -d '"' -f 2 | cut -d ',' -f 1 | sed -i '' 's/^/\\\"/;s/$/\\\"/'
