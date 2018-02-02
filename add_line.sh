7a0ebe326d26700063746225d77c4e46dfad4e84
Clementine <clementine@MacBook-Pro-de-Clementine.local>
2018-02-02 14:09:54 +0100

    Upd : final script

--- add_line.sh
+++ add_line.sh
@@ -8,26 +8,17 @@ insert_line() {
 	echo "ft_write_debug($str_test, data, ret); in $1"
 }
 
-#delete_line() {
-#	line=`grep "ft_write_debug" $1
-#	sed "/$line/d" $1
-#}
+delete_line() {
+	line=`grep "ft_write_debug" $1`
+	sed "/$line/d" $1
+}
 
-#action_on_file() {
-	
-	#insert_line $1
-#}
+files=`find . -type d \( -name "conv_*" -o -name "basics" -o -name "percent" -o -name "mixed" \) \
+	-exec find {} -name "*.c" \;`
 
-insert_line $1
-
-#files=`find . -type d \( -name "conv_*" -o -name "basics" -o -name "percent" -o -name "mixed" \) \
-#	-exec find {} -name "*.c" \;`
-
-#for i in $files; do
-#	action_on_file $i
-#done
-
-#delete_line
+for i in $files; do
+	insert_line $i
+done
 
 ### Add '\"' but sed -i  doesn't work after the grep
 #grep 'write_debug' < conv_s/a14_all_02.c | cut -d '"' -f 2 | cut -d ',' -f 1 | sed -i '' 's/^/\\\"/;s/$/\\\"/'
