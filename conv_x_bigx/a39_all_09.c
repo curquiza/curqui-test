50d1eb19bba1d5d62bed146e31349af347943768
Réouven Sellam <reouven.sellam@gmail.com>
2018-03-10 21:40:22 +0100

    Create a39_all_09.c

---
+++ conv_x_bigx/a39_all_09.c
@@ -0,0 +1,26 @@
+#include "conv_x_bigx_test.h"
+
+int		x_all_09(void)
+{
+	t_data	data;
+	int		pfd[2];
+	int		ret;
+	int		save_stdout;
+
+	ft_write_test_name("%0#10.0x, 0");
+	ft_connect_stdout(pfd, &save_stdout);
+	data.r1 = ft_printf("%0#10.0x", 0);
+	data.s1 = ft_get_stdout(pfd, &save_stdout);
+	ft_connect_stdout(pfd, &save_stdout);
+	data.r2 = printf("%0#10.0x", 0);
+	data.s2 = ft_get_stdout(pfd, &save_stdout);
+	ret = 0;
+	if (data.r1 != data.r2)
+		ret = -1;
+	if (ft_strcmp(data.s1, data.s2))
+		ret = -1;
+	ft_write_rslt(data, ret);
+	ft_strdel(&data.s1);
+	ft_strdel(&data.s2);
+	return (ret);
+}
